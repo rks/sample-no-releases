1be6d5d4c99c716499c348d8a677229c9c72fa3f
Randy Souza <randy@randysouza.com>
2021-04-15 15:33:58 -0400

    Update repository.m

--- repository.m
+++ repository.m
@@ -1,4 +1,4 @@
 function repositoryMetadata = repository
-    repositoryMetadata.name = "https://github.com/rks/sample.git";
+    repositoryMetadata.name = "https://github.com/rks/sample-no-releases.git";
 end
 
